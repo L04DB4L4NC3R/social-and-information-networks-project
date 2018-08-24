130ac5b3427c85ca8a8d9faaa4c44381d5961ed3
angad <angadsharma1016@gmail.com>
2018-08-25 01:01:36 +0530

    user input pass and username

--- main.js
+++ main.js
@@ -6,11 +6,25 @@ const {
     click
 } = require("selenium-webdriver");
 
+const readline = require("readline");
+
+var rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+
+
+
+var username = "";
+var password = "";
+
+
+
+
 
 let func = async ()=>{
     let driver = await new Builder().forBrowser("firefox").build();
-    let username = "";
-    let password = "";
 
     try{
         await driver.get("https://twitter.com/login");
@@ -26,7 +40,7 @@ let func = async ()=>{
 
         let arr = [],temp = '';
 
-        console.log("Your followers are: \n");
+        console.log("\n\nYour followers are: \n");
 
         for(el of list){
             temp = await el.getAttribute("href");
@@ -48,4 +62,26 @@ let func = async ()=>{
 
 
 
-func();
+
+
+
+
+
+
+rl.question("Enter your twitter username:  ",(res)=>{
+    rl.stdoutMuted = true;
+    rl.question("Password:  ",(resp)=>{
+       username = res;
+       password = resp;
+       rl.close();
+       func();
+    });
+});
+
+
+rl._writeToOutput = function _writeToOutput(stringToWrite) {
+    if (rl.stdoutMuted)
+      rl.output.write("\x1B[2K\x1B[200D"+"Password:  "+"["+((rl.line.length%2==1)?"=-":"-=")+"]");
+    else
+      rl.output.write(stringToWrite);
+  };
